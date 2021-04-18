5b08c7f30731975ec181a347f659b03d7b083754
Tushar281997 <78564584+Tushar281997@users.noreply.github.com>
2021-04-18 14:53:54 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ def convert_html_to_pdf(source_html_path, output_filename, context=None):
     """
     :param source_html_path: path of the html file
     :param output_filename: name of the output file
-    :param context: list of data which need to be passed as context
+    :param context: dictionary of data which need to be passed as context
     :return: outputfile path
     """
     # open output file for writing (truncated binary)
