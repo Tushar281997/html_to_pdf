c9550d74e9c428b4ef79290b47129566921d451a
Tushar281997 <78564584+Tushar281997@users.noreply.github.com>
2021-04-18 15:05:54 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ def convert_html_to_pdf(source_html_path, output_filename, context=None):
     :param source_html_path: path of the html file
     :param output_filename: name of the output file
     :param context: dictionary of data which need to be passed as context
-    :return: outputfile path
+    :return: Boolen
     """
     # open output file for writing (truncated binary)
     source_html = open(source_html_path).read()
@@ -25,4 +25,4 @@ def convert_html_to_pdf(source_html_path, output_filename, context=None):
     result_file.close()  # close output file
 
     # return False on success and True on errors
-    return output_filename
+    return pisa_status
