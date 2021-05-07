366f0d27866e65df8fd3b79b70de12b56861e9e7
Tushar281997 <78564584+Tushar281997@users.noreply.github.com>
2021-05-07 13:14:58 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,25 +3,23 @@ from xhtml2pdf import pisa
 
 def convert_html_to_pdf(source_html_path, output_filename, context=None):
     """
-    :param source_html_path: path of the html file
-    :param output_filename: name of the output file
-    :param context: dictionary of data which need to be passed as context
-    :return: Boolen
-    """
-    # open output file to read
-    source_html = open(source_html_path).read()
+       :param source_html_path: path of the html file
+       :param output_filename: name of the output file
+       :param context: dictionary of data which need to be passed as context
+       :return: file name
+       """
+    # open output file for writing (truncated binary)
+    source_html = open(source_html_path,"r",encoding='utf-8').read()
     if context:
         context_keys = context.keys()
         for context_key in context_keys:
             source_html = source_html.replace(context_key, context.get(context_key))
     result_file = open(output_filename, "w+b")
     # convert HTML to PDF
-    pisa_status = pisa.CreatePDF(
-        source_html,  # the HTML to convert
-        dest=result_file)  # file handle to recieve result
+    pisa_status = pisa.CreatePDF(source_html.encode('utf-8'), dest=result_file,
+                                encoding='utf-8')
 
     # close output file
     result_file.close()  # close output file
 
-    # return False on success and True on errors
-    return pisa_status
+    return output_filename
